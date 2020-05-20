a993cb6e93fbd5984652db97b45bb39c3fefc495
Alexey <Elesin>
2020-05-20 18:09:26 +0300

    fix copy/paste in non english keyboard layouts

--- navi_curve_helper.py
+++ navi_curve_helper.py
@@ -43,6 +43,18 @@ def read_from_file(window):
                 window[key].update(config[key])
 
 
+def _onKeyRelease(event):
+    ctrl  = (event.state & 0x4) != 0
+    if event.keycode==88 and  ctrl and event.keysym.lower() != "x":
+        event.widget.event_generate("<<Cut>>")
+
+    if event.keycode==86 and  ctrl and event.keysym.lower() != "v":
+        event.widget.event_generate("<<Paste>>")
+
+    if event.keycode==67 and  ctrl and event.keysym.lower() != "c":
+        event.widget.event_generate("<<Copy>>")
+
+
 
 sg.theme('SystemDefault')
 
@@ -111,6 +123,7 @@ layout = [[sg.Frame(layout=[[a_stock, b_stock, c_stock]],
 
 window = sg.Window('Navi Curve Helper', layout=layout, icon='navi_offset_curve.ico')
 window.finalize()
+window.TKroot.bind_all("<Key>", _onKeyRelease, "+")
 read_from_file(window)
 while True:
     event, values = window.read()
